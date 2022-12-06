a1fdfa862966c6890a375b547ae57d36e987e040
joanjanku2000 <janku.joan4@gmail.com>
2022-12-07 00:02:23 +0100

    Refactor -> Replaced those nasty try..catch blocks with a beautiful function with lambda expression params

--- src/main/java/com/jpa/filter/dao/RepoUtil.java
+++ src/main/java/com/jpa/filter/dao/RepoUtil.java
@@ -1,8 +1,11 @@
 package com.jpa.filter.dao;
 
-import com.jpa.filter.dto.filter.Filter;
+import com.jpa.filter.dto.Filter;
+import com.jpa.filter.helper.ExceptionMessages;
+import com.jpa.filter.helper.Executable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import javax.persistence.criteria.*;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
@@ -11,8 +14,8 @@ import java.time.LocalDateTime;
 import java.time.chrono.ChronoLocalDate;
 import java.time.chrono.ChronoLocalDateTime;
 import java.time.chrono.Chronology;
-import java.time.format.DateTimeParseException;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 public class RepoUtil {
@@ -56,15 +59,15 @@ public class RepoUtil {
      *     <li>Validates if the provided data type corresponds with the field's datatype</li>
      *     <li>Extracts the predicate for the queried request</li>
      * </ol>
-     *
+     * <p>
      *  At the moment only 4 data types are supported: Numeric i.e Double , String , LocalDate , LocalDateTime
+     *
      * @param filter {@link Filter}
-     * @param cb {@link CriteriaBuilder}
-     * @param root {@link Root}
-     * @param clazz {@link Class}
+     * @param cb     {@link CriteriaBuilder}
+     * @param root   {@link Root}
      * @return {@link Predicate}
      */
-    public static <T> Predicate extractCriteria(Filter filter, CriteriaBuilder cb, Root<T> root, Class clazz) {
+    public static <T> Predicate extractCriteria(Filter filter, CriteriaBuilder cb, Root<T> root) {
         logger.info("Extracting criteria ... ");
 
         Join<Object, Object> joinObject = null;
@@ -76,7 +79,6 @@ public class RepoUtil {
             logger.info("Join java type ");
             joinObject = getJoinObject(root, joinObject, nestedFields);
             logger.info("Join java type {} ", joinObject.getJavaType());
-            clazz = joinObject.getJavaType();
         }
 
         Path<Double> doublePath;
@@ -86,70 +88,70 @@ public class RepoUtil {
 
         double doubleValue;
         String stringValue;
-        ChronoLocalDate lDateValue;
-        ChronoLocalDateTime lDateTimeValue;
+        AtomicReference<ChronoLocalDate> lDateValue = new AtomicReference<>();
+        AtomicReference<ChronoLocalDateTime> lDateTimeValue = new AtomicReference<>();
+
 
         Predicate predicate = null;
         switch (filter.getType()) {
             case NUMERIC:
                 doublePath = joinObject != null ? joinObject.get(filter.getField().split("[.]")[nestedFields.size() - 1]) : root.get(filter.getField());
+                doubleValue =  execute(() ->
+                                Double.parseDouble(filter.getValue())
+                                , ExceptionMessages.VALUE_IS_NOT_DOUBLE);
 
-                try {
-                    doubleValue = Double.parseDouble(filter.getValue());
-                } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("Provided value isn't of type Double");
+                if (!doublePath.getJavaType().equals(Double.class)) {
+                    throw new IllegalArgumentException("Field " + filter.getField() + " should be of type Double , found " + doublePath.getJavaType());
                 }
-                if (!doublePath.getJavaType().equals(Double.class)){
-                    throw new IllegalArgumentException("Field " + filter.getField() + " should be of type Double , found "+ doublePath.getJavaType());
-                }
+
                 predicate = getPredicate(filter, cb, doublePath, doubleValue);
                 break;
             case STRING:
                 stringPath = joinObject != null ? joinObject.get(filter.getField().split("[.]")[nestedFields.size() - 1]) : root.get(filter.getField());
                 stringValue = filter.getValue();
-
                 if (!stringPath.getJavaType().equals(String.class)) {
                     throw new IllegalArgumentException("Field " + filter.getField() + " should be of type String , found " + stringPath.getJavaType().getSimpleName());
                 }
+
                 predicate = getPredicate(filter, cb, stringPath, stringValue);
                 break;
-
             case LOCAL_DATE:
                 datePath = joinObject != null ? joinObject.get(filter.getField().split("[.]")[nestedFields.size() - 1]) : root.get(filter.getField());
-
-                if (!datePath.getJavaType().equals(LocalDate.class)){
+                if (!datePath.getJavaType().equals(LocalDate.class)) {
                     throw new IllegalArgumentException("Field " + filter.getField() + " should be of type LocalDate , found " + datePath.getJavaType());
                 }
 
-                try {
+                lDateValue.set( execute(() -> {
                     Chronology chronology = Chronology.ofLocale(Locale.ENGLISH);
-                    lDateValue = chronology.date(LocalDate.parse(filter.getValue()));
-                } catch (DateTimeParseException e) {
-                    throw new IllegalArgumentException("Provided value isn't of type LocalDate , it should comply with: YYYY-MM-DD");
-                }
-                predicate = getPredicate(filter, cb, datePath, lDateValue);
-                break;
+                    return chronology.date(LocalDate.parse(filter.getValue()));
 
+                }, ExceptionMessages.VALUE_IS_NOT_LOCAL_DATE));
+                predicate = getPredicate(filter, cb, datePath, lDateValue.get());
+                break;
             case LOCAL_DATE_TIME:
                 ldateTimePath = joinObject != null ? joinObject.get(filter.getField().split("[.]")[nestedFields.size() - 1]) : root.get(filter.getField());
-
-                if (!ldateTimePath.getJavaType().equals(LocalDateTime.class)){
+                if (!ldateTimePath.getJavaType().equals(LocalDateTime.class)) {
                     throw new IllegalArgumentException("Field " + filter.getField() + " should be of type LocalDateTime");
                 }
-                try {
-                    Chronology chronology = Chronology.ofLocale(Locale.ENGLISH);
-                    lDateTimeValue = chronology.localDateTime(LocalDateTime.parse(filter.getValue()));
-                } catch (DateTimeParseException e) {
-                    throw new IllegalArgumentException("Provided value isn't of type LocalDateTime, it should comply with: YYYY-MM-DD T hh:mm:ss");
-                }
-                predicate = getPredicate(filter, cb, ldateTimePath, lDateTimeValue);
-                break;
 
+                lDateTimeValue.set(execute(() -> {
+                    Chronology chronology = Chronology.ofLocale(Locale.ENGLISH);
+                    return chronology.localDateTime(LocalDateTime.parse(filter.getValue()));
+                }, ExceptionMessages.VALUE_IS_NOT_LOCAL_DATE_TIME));
+                predicate = getPredicate(filter, cb, ldateTimePath,  lDateTimeValue.get());
         }
+
         return predicate;
 
     }
 
+    private static <T> T execute(Executable executable, String message) {
+        try {
+            return (T) executable.execute();
+        } catch (Exception var3) {
+            throw new IllegalArgumentException(message);
+        }
+    }
 
 
     private static <T extends Comparable<T>> Predicate getPredicate(Filter filter, CriteriaBuilder cb, Path<T> doublePath, T doubleValue) {
